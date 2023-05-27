61c1d6a37facab3d1ceabf954365b35c73c035c7
Szymon Truszkowski <struszkowski98@gmail.com>
2023-05-27 16:49:37 +0200

    comments added in the Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,10 +1,22 @@
 public class Customer {
 
+    /**
+     * The first name of this customer.
+     */
     private String firstName;
 
+    /**
+     * The last name of this customer.
+     */
     private String lastName;
 
+    /**
+     * The id number of this customer.
+     */
     private String id;
 
+    /**
+     * The password of this customer.
+     */
     private String password;
 }
