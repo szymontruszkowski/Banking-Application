1f7e661f3a039b1fc5731fd592a27a025ba9fd59
Szymon Truszkowski <struszkowski98@gmail.com>
2023-05-28 10:52:20 +0200

    comments added in the Account class

--- src/Account.java
+++ src/Account.java
@@ -1,10 +1,22 @@
 public class Account {
 
+    /**
+     * The type of this account.
+     */
     private String type;
 
+    /**
+     * The id number of this account.
+     */
     private String id;
 
+    /**
+     * The currency of this account.
+     */
     private String currency;
 
+    /**
+     * The owner of this account.
+     */
     private Customer owner;
 }
