f3dd5b34cc52cf91ad77185a5814d67bd55f875f
Szymon Truszkowski <struszkowski98@gmail.com>
2023-05-29 06:30:27 +0200

    comments added in the Transaction class

--- src/Transaction.java
+++ src/Transaction.java
@@ -2,11 +2,23 @@ import java.util.Date;
 
 public class Transaction {
 
+    /**
+     * The amount of this transaction.
+     */
     private double amount;
 
+    /**
+     * The date of this transaction.
+     */
     private Date date;
 
+    /**
+     * The title of this transaction.
+     */
     private String title;
 
+    /**
+     * The bank account in which this transaction is executed.
+     */
     private Account account;
 }
