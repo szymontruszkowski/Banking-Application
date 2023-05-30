03212400e2661324fc11bf4055d1ce59baeaf670
Szymon Truszkowski <struszkowski98@gmail.com>
2023-05-30 15:20:35 +0200

    comments added in the Bank class

--- src/Bank.java
+++ src/Bank.java
@@ -2,9 +2,18 @@ import java.util.List;
 
 public class Bank {
 
+    /**
+     * The name of this bank.
+     */
     private String name;
 
+    /**
+     * The list of customers of this bank.
+     */
     private List<Customer> customers;
 
+    /**
+     * The list of accounts of this bank.
+     */
     private List<Account> accounts;
 }
