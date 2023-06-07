af944cb8252f8091887bc59c22ef53f5b9cb7fb8
Szymon Truszkowski <struszkowski98@gmail.com>
2023-06-07 16:21:21 +0200

    main() method updated in the Application class

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Application {
+
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         Bank theBank = new Bank("STBA");
-        
+
+        Customer verifiedCustomer;
+
+        while (true) {
+            signInUp(theBank);
+            verifiedCustomer = signIn(theBank);
+            printMainMenu(theBank, verifiedCustomer);
+        }
     }
 }
