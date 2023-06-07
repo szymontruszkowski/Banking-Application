9ac422f7f3cc75e70160e6761cef978363a2e09e
Szymon Truszkowski <struszkowski98@gmail.com>
2023-06-07 16:36:07 +0200

    signInUp() method added in the Application class

--- src/Application.java
+++ src/Application.java
@@ -6,7 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Bank theBank = new Bank("STBA");
+        Bank theBank = new Bank("Szymon's Bank");
 
         Customer verifiedCustomer;
 
@@ -14,6 +14,36 @@ public class Application {
             signInUp(theBank);
             verifiedCustomer = signIn(theBank);
             printMainMenu(theBank, verifiedCustomer);
+        }
+    }
+
+    public static void signInUp(Bank theBank) {
+
+        int choice;
+
+        System.out.printf("\n\n * * * Welcome in %s * * *\n\n", theBank.getName());
+        do {
+            System.out.println("  1. Sign in");
+            System.out.println("  2. Sign up");
+            System.out.print("Select the option (1-2): ");
+
+            choice = scanner.nextInt();
+            if (choice < 1 || choice > 2) {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice < 1 || choice > 2);
+
+        switch (choice) {
+            case 1:
+                scanner.nextLine();
+                break;
+            case 2:
+                signUp(theBank);
+                break;
+        }
+
+        if (choice != 1) {
+            signInUp(theBank);
         }
     }
 }
